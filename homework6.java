933e70ad679bd4c97446d8897e5cbd73d6d7f208
veromax3 <veromax3@gmail.com>
2023-06-25 15:27:04 +0300

    added homework 6

---
+++ homework6.java
@@ -0,0 +1,37 @@
+public class homework6 {
+    public static void main(String[] args) {
+        Notebook notebook1 = new Notebook();
+        notebook1.RAM = 16;
+        notebook1.HDD_SSD = 500; 
+        notebook1.OS = "MS Windows";
+        notebook1.colour = "white";
+
+        Notebook notebook2 = new Notebook();
+        notebook2.RAM = 8;
+        notebook2.HDD_SSD = 250; 
+        notebook2.OS = "MS Windows";
+        notebook2.colour = "black";
+
+        Notebook notebook3 = new Notebook();
+        notebook3.RAM = 8;
+        notebook3.HDD_SSD = 256; 
+        notebook3.OS = "MacOS";
+        notebook3.colour = "black";
+
+        Notebook notebook4 = new Notebook();
+        notebook4.RAM = 32;
+        notebook4.HDD_SSD = 1000; 
+        notebook4.OS = "MacOS";
+        notebook4.colour = "grey";
+
+        Notebook notebook5 = new Notebook();
+        notebook5.RAM = 8;
+        notebook5.HDD_SSD = 512; 
+        notebook5.OS = "Linux";
+        notebook5.colour = "grey";
+
+
+
+    }
+    
+}
