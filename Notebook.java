24feda601b81d7525f062d5c0c2db86e1083d3ef
veromax3 <veromax3@gmail.com>
2023-07-08 00:33:38 +0300

    added notebooks

--- Notebook.java
+++ Notebook.java
@@ -6,24 +6,10 @@ public class Notebook {
     String OS;
     String colour;
 
-
-    // public Notebook(String name, int RAM,int HDD_SSD, String OS, String colour ){
-    //     this.name = name;
-    //     this.RAM = RAM;
-    //     this.HDD_SSD = HDD_SSD;
-    //     this.OS = OS;
-    //     this.colour = colour;
-    // }
-
     @Override
     public boolean equals(Object obj) {
         Notebook t = (Notebook) obj;
         return RAM == t.RAM || HDD_SSD == t.HDD_SSD || OS == t.OS || colour == t.colour;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("name:%s RAM:%d HDD/SSD:%d OS:%s colour:%s", name, RAM, HDD_SSD, OS, colour);
     }
 }
 
